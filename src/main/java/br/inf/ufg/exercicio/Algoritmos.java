3442231e9c2d10b839292668445e0b346a77d8af
Jean Lucas Monte Carvalho <jean.rv@hotmail.com>
2016-10-02 12:32:38 -0300

    Algoritmos documentados em javadoc e testados com jacoco

--- src/main/java/br/inf/ufg/exercicio/Algoritmos.java
+++ src/main/java/br/inf/ufg/exercicio/Algoritmos.java
@@ -1,5 +1,10 @@
 package br.inf.ufg.exercicio;
 
+/**
+ * Algoritmos definidos como exercicio para aula2.
+ * 
+ * @author Jean
+ */
 public class Algoritmos {
 
 	/**
@@ -9,6 +14,7 @@ public class Algoritmos {
 	 *            valor de x para divisao
 	 * @param y
 	 *            valor de y para divisao
+	 * @return resto da divisao
 	 * 
 	 */
 	public static int mod(int x, int y) {
@@ -22,8 +28,17 @@ public class Algoritmos {
 		return s;
 	}
 
+	/**
+	 * Algoritmo para validação de cpf
+	 * 
+	 * @param d
+	 *            parametro com o numero do cpf
+	 * @return valor true or false dependendo da validade do cpf
+	 */
 	public static boolean cpf(int d[]) {
-
+		if (d.length > 11) {
+			throw new IllegalArgumentException("Numero de cpf maior que 11 digitos");
+		}
 		int sj = d[0] + 2 * d[1] + 3 * d[2] + 4 * d[3] + 5 * d[4] + 6 * d[5] + 7 * d[6] + 8 * d[7] + 9 * d[8];
 		int sk = d[1] + 2 * d[2] + 3 * d[3] + 4 * d[4] + 5 * d[5] + 6 * d[6] + 7 * d[7] + 8 * d[8] + 9 * d[9];
 		int j = mod(mod(sj, 11), 10);
@@ -32,7 +47,17 @@ public class Algoritmos {
 		return (j == d[9]) && (k == d[10]);
 	}
 
+	/**
+	 * Algoritmo para validação de cpf
+	 * 
+	 * @param d
+	 *            parametro com o numero do cpf
+	 * @return valor true or false dependendo da validade do cpf
+	 */
 	public static boolean cpf2(int d[]) {
+		if (d.length > 11) {
+			throw new IllegalArgumentException("Numero de cpf maior que 11 digitos");
+		}
 		int c = 7;
 		int p = d[8];
 		int s = d[8];
--- src/test/java/br/inf/ufg/exercicio/AlgoritmosTest.java
+++ src/test/java/br/inf/ufg/exercicio/AlgoritmosTest.java
@@ -6,10 +6,44 @@ import org.junit.Test;
 
 public class AlgoritmosTest {
 
+	// Testes para funcao cpf
 	@Test
 	public void CPFValidoTest() {
-		int [] d = {0,3,7,7,1,7,6,3,1,1,8};
+		int[] d = { 0, 3, 7, 7, 1, 7, 6, 3, 1, 1, 8 };
+		assertTrue(Algoritmos.cpf(d));
+	
+	}
+
+	@Test
+	public void CPFInValidoTest() {
+		int[] d = { 0, 0, 0, 7, 1, 7, 6, 3, 1, 1, 8 };
+		assertFalse(Algoritmos.cpf(d));
+	
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void CPFInValido12DigitosTest() {
+		int[] d = { 0, 0, 0, 7, 1, 7, 6, 3, 1, 1, 8, 1 };
+		Algoritmos.cpf(d);
+	}
+
+	// Testes para funcao cpf2
+	@Test
+	public void CPF2ValidoTest() {
+		int[] d = { 0, 3, 7, 7, 1, 7, 6, 3, 1, 1, 8 };
 		assertTrue(Algoritmos.cpf2(d));
+	}
+
+	@Test
+	public void CPF2InValidoTest() {
+		int[] d = { 0, 0, 0, 7, 1, 7, 6, 3, 1, 1, 8 };
+		assertFalse(Algoritmos.cpf2(d));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void CPF2InValido12DigitosTest() {
+		int[] d = { 0, 0, 0, 7, 1, 7, 6, 3, 1, 1, 8, 1 };
+		Algoritmos.cpf2(d);
 	}
 
 }
